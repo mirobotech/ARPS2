0ba116137dae021a8ef52f13887f961fa672557c
John Rampelt <johnrampelt@gmail.com>
2026-02-06 18:34:38 -0500

    Update Beginner Activity 3

---
+++ Beginner Actvitities/Activity-B03-Input/ARPS2.h
@@ -0,0 +1,99 @@
+/* =============================================================================
+ARPS2.h
+February 1, 2026
+
+Board header file for the mirobo.tech ARPS-2 circuit.
+
+This header file defines all of the ARPS-2 I/O devices allowing
+beginners to focus on learning programming concepts more quickly.
+
+ARPS-2 hardware notes:
+- Pushutton switches use internal pull-up resistors (so pressed == LOW)
+- LEDs and motor driver share I/O pins
+- Headers H1-H4 are shared between digital I/O, analog I/O, I2C, and SONAR
+==============================================================================*/
+
+#ifndef ARPS2_H
+#define ARPS2_H
+
+// Pre-defined Arduino UNO LED
+// LED_BUILTIN (D13)      // On-board LED (shared with H2 and SONAR TRIG)
+
+/* =====================================
+ * Pushbutton Pins (Active LOW)
+ * ====================================*/
+// NOTE: SW2 and SW3 are fully supported on Arduion UNO R4 Minima and Arduino 
+// UNO R4 WiFi. Arduino UNO Rev 3 uses D0 and D1 as serial Rx and Tx lines
+// which prevents SW2 and SW3 from operating during programming and debugging.
+
+const uint8_t SW2 = 0;    // UNO R4 only!
+const uint8_t SW3 = 1;    // UNO R4 only!
+const uint8_t SW4 = 2;
+const uint8_t SW5 = 4;
+
+
+/* =====================================
+ * LED Pins
+ * ====================================*/
+// IMPORTANT: LED pins are shared with the motor controller. Using the
+// LEDs while the the motors are active will affect motor behavior!
+
+const uint8_t LED2 = 3;   // M1A
+const uint8_t LED3 = 9;   // M1B
+const uint8_t LED4 = 10;  // M2A
+const uint8_t LED5 = 11;  // M2B
+
+
+/* =====================================
+ * Motor Pins
+ * ====================================*/
+// ARPS2 motor output pin definitions (redefines same I/O pins as LEDs)
+
+const uint8_t M1A = 3;    // Left motor terminal A
+const uint8_t M1B = 9;    // Left motor terminal B
+const uint8_t M2A = 10;   // Right motor terminal A
+const uint8_t M2B = 11;   // Right motor terminal B
+
+
+/* =====================================
+ * Piezo Beeper Pin
+ * ====================================*/
+
+const uint8_t LS1 = 6;    // ARPS2 piezo beeper LS1
+
+
+/* =====================================
+ * Expansion Header I/O Pins
+ * ====================================*/
+// NOTE: I/O headers H1 and H4 are shared with analog inputs A4 (AH1) and A5 (AH4).
+
+const uint8_t H1 = 18;    // Header H1 (digital, shared with A4)
+const uint8_t H2 = 13;    // Header H2 (shared with SONAR TRIG and onboardLED)
+const uint8_t TRIG = 13;  // Ultrasonic SONAR distance sensor TRIG(ger) output
+const uint8_t H3 = 12;    // Header H3 (shared with SONAR ECHO)
+const uint8_t ECHO = 12;  // Ultrasonic SONAR distance sensor ECHO input
+const uint8_t H4 = 19;    // Header H4 (digital, shared with A5)
+const uint8_t H9 = 7;     // Digital I/O or Servo output pin
+const uint8_t H10 = 8;    // Digital I/O or Servo output pin
+
+
+/* =====================================
+ * Optional IR Demodulator Input Pin
+ * ====================================*/
+
+const uint8_t IR = 5;     // Demodulator U3
+
+/* =====================================
+ * Analog I/O Pins
+ * ====================================*/
+// NOTE: Analog inputs AH1 and AH4 are shared with D18, D19, and I2C SDA, SCL
+
+const uint8_t Q1 = A0;    // Left floor/line sensor phototransistor Q1
+const uint8_t Q2 = A1;    // Right line sensor phototransistor Q2
+const uint8_t Q3 = A1;    // Right floor sensor phototransistor Q3
+const uint8_t ATEMP = A2; // Analog temperature sensor U1
+const uint8_t ADIV = A3;  // Analog voltage divider (R20/R21) tap
+const uint8_t AH1 = A4;   // Analog input from header H1 (shared with D18)
+const uint8_t AH4 = A5;   // Analog input from header H4 (shared with D19)
+
+#endif
---
+++ Beginner Actvitities/Activity-B03-Input/Activity-B03-Input.ino
@@ -0,0 +1,219 @@
+/* ================================================================================
+Beginner Activity 3: Input [Activity-B03-Input]
+February 6, 2026
+
+Platform: mirobo.tech ARPS-2 circuit (any configuration)
+Requires: ARPS2.h header file
+=================================================================================*/
+// IMPORTANT: Make sure that ARPS2.h is accerssible as a tab in this project.
+#include "ARPS2.h"      // Define ARPS2 I/O devices
+
+// ---------------------------------------------------------------
+// Setup runs once at power-up
+// ---------------------------------------------------------------
+void setup()
+{
+    // Configure Arduino Nano ESP32 status LED as output
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);    // Status LED on
+
+    // Configure ARPS2 LEDs as outputs
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
+    pinMode(LED5, OUTPUT);
+
+    // Configure ARPS2 pushbuttons as inputs
+    pinMode(SW2, INPUT_PULLUP);
+    pinMode(SW3, INPUT_PULLUP);
+    pinMode(SW4, INPUT_PULLUP);
+    pinMode(SW5, INPUT_PULLUP);
+}
+
+// ---------------------------------------------------------------
+// Loop runs forever
+// ---------------------------------------------------------------
+void loop()
+{
+    // Momentary button SW2
+    if (digitalRead(SW2) == LOW)
+    {
+        digitalWrite(LED2, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED2, LOW);
+    }
+
+    // Start a pattern
+    if (digitalRead(SW5) == LOW)
+    {
+        digitalWrite(LED2, HIGH);
+        delay(200);
+        digitalWrite(LED3, HIGH);
+        delay(200);
+        digitalWrite(LED4, HIGH);
+        delay(200);
+        digitalWrite(LED5, HIGH);
+        delay(200);
+        digitalWrite(LED2, LOW);
+        delay(200);
+        digitalWrite(LED3, LOW);
+        delay(200);
+        digitalWrite(LED4, LOW);
+        delay(200);
+        digitalWrite(LED5, LOW);
+        delay(200);
+    }
+
+    delay(20);  // Add a short loop delay
+}
+
+
+/*
+Program Analysis Activities
+
+1.  The pushbuttons in the ARPS-2 circuit are connected in what is
+    known as a pull-up configuration, meaning one side of each
+    pushbutton switch is wired in series with a resistor that is
+    connected, or 'pulled-up', to the power supply potential. The
+    other terminal of the pushbutton switch is connected to ground,
+    and the microcontroller input pin is connected in-between the
+    pull-up resistor and the switch, so that it reads the potential
+    across the switch.
+    
+    The inactive potential (when the pushbutton is not pressed) of a
+    pull-up circuit will be a high voltage due to the resistor's power
+    supply connection, and the active potential (when the pushbutton is
+    pressed) will be 0V. This pull-up circuit arrangment creates what
+    is commonly referred to as an 'active-low' pushbutton switch.
+    
+    Arduino's digitalRead() function is used to read the value of
+    I/O pins, and it reads pins as being either HIGH or LOW (exactly
+    like the values set using the digitalWrite() function). Let's
+    look at the if-else structure in the program:
+
+    // Momentary button SW2
+    if (digitalRead(SW2) == LOW)
+    {
+        digitalWrite(LED2, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED2, LOW);
+    }
+
+    The 'if' statement, known as an 'if condition', employs a similar
+    structure to that used by the main loop(). That is, the program
+    statements indented below the if statement will execute whenever
+    the if condition is true. And, in this case the condition will be
+    true when the value of the SW2 pin is equal to LOW (two equals
+    signs are used to check and compare values, as oppposed to one
+    one equals sign being used to set a value).
+
+    This if condition also includes a complementary 'else' statement,
+    whcih will logically be the opposite of if. When if is true, else
+    will be false, and when if is false, else will become true.
+
+    Looking at SW2's if-else condition, explain the program flow when
+    SW2 is not pressed, and when SW2 is pressed. Which digitalWrite()
+    function executes in each case?
+
+2.  Press SW5 to start a light pattern. While the light pattern is
+    running, does pressing and holding SW2 turn LED2 on? Explain why
+    or why not, and what you think is happening in the program. Can
+    you think of a way to test and verify your assumption?
+
+3.  The previous programming activity demonstrated how to use the
+    tone() function to play a tone for a specific amount of time by
+    passing it both frequency and duration arguments. Passing the
+    tone() function only a frequency argument will cause it to start
+    playing the tone until either: another call to tone() changes the
+    frequency, or the noTone() function is called to stop the tone.
+
+    An if-else condition is an ideal way to play the tone while a
+    button is being pressed, and stop it when the button is released.
+    Add this code to your program to try it out:
+
+    if (digitalRead(SW3) == LOW)
+    {
+        tone(LS1, 440);
+    }
+    else
+    {
+        noTone(LS1);
+    }
+
+    What is the advantage of using an 'if-else' structure instead of
+    using two separate if conditions – one to start the tone, and a
+    second to stop the tone – to do the same thing?
+
+4.  Let's try using a combination of two pushbuttons to turn on one
+    LED. One way to accomplish this is by nesting one if-else
+    condition inside another, like this:
+
+    if (digitalRead(SW3) == LOW)
+    {
+        if (digitalRead(SW4) == LOW)
+        {
+            digitalWrite(LED3, HIGH);
+        }
+        else
+        {
+            digitalWrite(LED3, LOW);
+        }
+    }
+    else
+    {
+        digitalWrite(LED3, LOW);
+    }
+    
+    The nested if-else logic enables SW4 to turn the LED only if both
+    it and SW3 are pressed. Try the code in your program to verify
+    that it works as expected.
+
+5.  A better way to use two buttons to turn on one LED is by using
+    an '&&' (AND) logical operator in the if condition, like this:
+
+    if (digitalRead(SW3) == LOW && digitalRead(SW4) == LOW)
+    {
+        digitalWrite(LED3, HIGH);
+    }
+    else
+    {
+        digitalWrite(LED3, LOW);
+    }
+
+    Try this code in your program to verify that it works. In what
+    ways is this solution better than using nested if-else conditions?
+    
+6.  The logical '||' (OR) operator can also be used in conditional
+    expressions. Describe the when the LED would be lit if the SW3
+    and SW4 pushbutton inputs in the activity were combined using a
+    logical or operator instead of the and operator in the condition.
+
+
+Programming Activities
+
+1.  Create a program that simulates the separate 'Start' and 'Stop'
+    buttons that would be found on large, industrial machines. The
+    machine (simulated by an LED) should turn on when the 'Start'
+    button is pressed, and remain on until the 'Stop' button is
+    pressed.
+
+2.  Describe what happens in the Start/Stop program, above, if both
+    pushbuttons are held? Is the machine (LED) on, or off? Describe
+    what the program is doing.
+
+3.  Modify the program in activity 1, to only turn the LED on if the
+    'Start' button is pressed while the 'Stop' button is released.
+    
+4.  Modify the program in activity 1, above, to light the LED only
+    after the 'Start' button is held for longer than one second.
+
+5.  Create a program that uses each pushbutton to either display its
+    own, unique lighting pattern using any combination of LEDs, or to
+    play different tones (or even short tunes) when each button is
+    pressed.
+
+*/
